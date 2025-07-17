4b6361fc15a99854df97b51467db023d2531c24f
Taha daboussi <Taha_daboussi@hotmail.com>
2025-07-17 22:55:12 +0300

    kek

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,14 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Supercycle - NFT Collection by Ben & Blacken',
+  description: 'An NFT collection by Ben & Blacken from delivered.design, minting this summer on Abstract Chain',
   generator: 'v0.dev',
+  icons: {
+    icon: '/favicon.png',
+    shortcut: '/favicon.png',
+    apple: '/favicon.png',
+  },
 }
 
 export default function RootLayout({
